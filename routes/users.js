cd30501fcf4f5f06f7106784ece52a8aeec2acbf
Valerii Koshelev <58371774+koshelev-valerii@users.noreply.github.com>
2020-03-22 11:03:44 +0100

    Changed routes

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-const { getUser } = require('../controllers/users');
+const { getUserById } = require('../controllers/users');
 
-router.get('/users/me', getUser);
+router.get('/users/me', getUserById);
 
 module.exports = router;
